e1e89d47eb6bb06879af222d277deebcf022f38c
epukata <epsionsardine5@gmail.com>
2026-05-15 04:27:17 +0900

    reconfig host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,5 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
-  if ENV["RENDER_EXTERNAL_HOSTNAME"].present?
-    config.hosts << ENV["RENDER_EXTERNAL_HOSTNAME"]
-  end
+  config.hosts << "rails-app-o5d9.onrender.com"
 end
