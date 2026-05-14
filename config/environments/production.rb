f95cd74d271f862ba0304436915fe4837d6cbb8f
epukata <epsionsardine5@gmail.com>
2026-05-15 03:42:06 +0900

    Add host at config/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,6 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.hosts << "rails-app-o5d9.onrender.com"
 end
