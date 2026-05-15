07eab528620eccfbbbcf1aad43bdbe7b78dafe9f
epukata <epsionsardine5@gmail.com>
2026-05-15 13:56:07 +0900

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   scope "/api/v1" do
     resources :recruitments, only: [ :index, :create ]
-    get "run_migration", to: "recruitments#run_migration"
   end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
